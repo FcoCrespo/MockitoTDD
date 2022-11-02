0c60346bf566ff487bbfa7493b4cf8c17dac97e2
fcrespo <fcrespo@dimae.com>
2022-11-02 09:34:41 +0100

    first mockito commit

---
+++ src/main/java/edu/tdd/educacion/EducacionApplication.java
@@ -0,0 +1,13 @@
+package edu.tdd.educacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tdd/educacion/EducacionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tdd.educacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
